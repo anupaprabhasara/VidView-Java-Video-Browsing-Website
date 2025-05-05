316a28e4399fff1926d305c4d02750996a600d40
anupaprabhasara <info@anupa.lk>
2025-05-05 15:23:26 +0530

    Basic Setup

---
+++ src/main/java/com/vidview/util/ConnectionData.java
@@ -0,0 +1,8 @@
+package com.vidview.util;
+
+public class ConnectionData {
+
+    public static final String DATABASE = "vidview_db";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "123456";
+}
